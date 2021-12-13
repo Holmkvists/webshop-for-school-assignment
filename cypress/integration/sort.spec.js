f7727e6a878c20e123e33ff98b151ac705472f1d
Holmkvists <adamholmkvists@gmail.com>
2021-12-13 19:30:50 +0100

    sort tests

--- cypress/integration/sort.spec.js
+++ cypress/integration/sort.spec.js
@@ -5,14 +5,31 @@
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
 
+import { catalog } from "../../src/ts/models/product-catalog";
+
 describe("sort tests", () => {
-  it("should sort low to high", () => {
-    // Arrange
+  beforeEach(() => {
     cy.visit("http://localhost:1234/productdetails.html#");
+  });
+
+  it("should sort price low to high", () => {
+    // Arrange
 
     // Act
+
     cy.get("#sortButton").click();
     cy.get("#lowToHigh").click();
+
+    // Result
+  });
+
+  it("should sort price high to low", () => {
+    // Arrange
+
+    // Act
+
+    cy.get("#sortButton").click();
+    cy.get("#highToLow").click();
 
     // Result
   });
