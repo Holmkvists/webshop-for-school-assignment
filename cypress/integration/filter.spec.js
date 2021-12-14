87e0f986ddfcded3340dde2f9d310c8b3e9c8b2d
Holmkvists <adamholmkvists@gmail.com>
2021-12-14 10:20:14 +0100

    filter tests

--- cypress/integration/filter.spec.js
+++ cypress/integration/filter.spec.js
@@ -3,17 +3,21 @@ describe("filter tests", () => {
     cy.visit("http://localhost:1234/productdetails.html#");
   });
 
-  it("should filter category by women", () => {
+  it("should filter brand by adidas Performance", () => {
     cy.get("#filterButton").click();
-    cy.get("#categoriesFilter a").eq(1).click();
+    cy.get("#brandsFilter a").eq(2).click();
 
-    cy.get("div > h3").should("contain.text", "Wmns Waffle Trainer 2");
+    cy.get("div > .mt-1").should("contain.text", "adidas Performance");
   });
 
-  it("should filter brand by nike sportswear", () => {
+  it("should filter brand by nike sportswear & sort price low to high", () => {
     cy.get("#filterButton").click();
     cy.get("#brandsFilter a").eq(3).click();
 
-    cy.get("div > .mt-1").should("have.text", "Nike Sportswear");
+    cy.get("#sortButton").click();
+    cy.get("#lowToHigh").click();
+
+    cy.get("div > .mt-1").should("contain.text", "Nike Sportswear");
+    cy.get(".flex-col > .text-sm").first().should("have.text", "$99");
   });
 });
